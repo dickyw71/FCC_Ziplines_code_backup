f9079316802f332afef204cddb55c8a9a0173175
Richard Wheatley <dickyw71@users.noreply.github.com>
2016-06-01 20:20:14 +0100

    Expanded grid object properties.

--- TicTacToe_Game/script.js
+++ TicTacToe_Game/script.js
@@ -20,6 +20,8 @@ $(document).ready(function() {
     }
     $('li').slideUp();
     $('#game').show();
+    // start game
+    
   });
 
   $('#game').click(function(event) {
@@ -75,6 +77,29 @@ function clearGame() {
   }
 }
 
+function startGame() {
+  // User or Computer goes first?
+  // Computer starts
+  
+  if(ticTacToe.computersTurn) {
+    
+    ticTacToe.takeTurn();
+  }
+}
+
+function chooseMove() {
+  // choose free square with best probability of making a winning line 
+  
+}
+
+function isPositionFree(square) {
+  // Look-up square row and column
+  // getRow(square) getColumn(square)
+  //ticTacToe.grid.find(square => ) 
+}
+
+
+
 function drawGrid() {
   var canvas = document.getElementById('game');
   var ctx = canvas.getContext('2d');
@@ -121,21 +146,26 @@ var ticTacToe = {
   "row": 0,
   "col": 0,
   "side": 'X',
-  "grid": [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8]
-  ],
+  "grid": [{num:'0', pos:[0,0], coords:[52,52], score:'3'}, 
+           {num:'1', pos:[0,1], coords:[152,52], score:'2'}, 
+           {num:'2', pos:[0,2], coords:[252,52], score:'3'},
+           {num:'3', pos:[1,0], coords:[52,152], score:'2'}, 
+           {num:'4', pos:[1,1], coords:[152,152], score:'4'}, 
+           {num:'5', pos:[1,2], coords:[252,152], score:'2'},
+           {num:'6', pos:[2,0], coords:[52,252], score:'3'}, 
+           {num:'7', pos:[2,1], coords:[152,252], score:'2'}, 
+           {num:'8', pos:[2,2], coords:[252,252], score:'3'}],
   "winningLines": [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
+    ['0', '1', '2'],
+    ['3', '4', '5'],
+    ['6', '7', '8'],
+    ['0', '3', '6'],
+    ['1', '4', '7'],
+    ['2', '5', '8'],
+    ['0', '4', '8'],
+    ['2', '4', '6']
   ],
+  // "preferredSquares": [ {4:[4]}, {3:[0,2,6,8]}, {2:[1,3,5,7]} ], 
   "game": [
     ['', '', ''],
     ['', '', ''],
